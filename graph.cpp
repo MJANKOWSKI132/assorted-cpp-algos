7231829e1a4e2093ea7098b7cc82ea64057aa729
Michael Jankowski <michael.jankowski@archtis.com>
2023-04-29 03:14:16 +1000

    Some changes

--- graph.cpp
+++ graph.cpp
@@ -3,15 +3,30 @@
 #include "ListNode.h"
 #include <memory>
 #include <queue>
+#include "priority-queue.h"
+#include <string>
 
 using std::vector;
 using std::queue;
+using std::pair;
 
-typedef std::unique_ptr<vector<int>> IntVector;
+typedef std::unique_ptr<vector<pair<int, int>>> AdjacentWeightVector;
+
+struct EdgeWeightComparator {
+    int operator() (const pair<int, int>& p1, const pair<int, int>& p2) {
+        return p2.second - p1.second;
+    }
+};
+
+struct DisplayEdge {
+    std::string operator() (const pair<int, int>& p) {
+        return "(" + std::to_string(p.first + 1) + ", " + std::to_string(p.second) + ")";
+    }
+};
 
 class Graph {
 private:
-    std::unique_ptr<vector<IntVector>> adj;
+    std::unique_ptr<vector<AdjacentWeightVector>> adj;
     bool directed;
     int numVertices;
 
@@ -21,33 +36,33 @@ private:
             return;
         }
         for (const auto& adjacent : *(*adj)[current]) {
-            if (!visited[adjacent]) {
-                visited[adjacent] = true;
-                dfsUtil(adjacent, target, visited, res);
+            if (!visited[adjacent.first]) {
+                visited[adjacent.first] = true;
+                dfsUtil(adjacent.first, target, visited, res);
             }
         }
     }
 public:
     Graph(const bool& directed, const int& numVertices) {
         this -> directed = directed;
-        this -> adj = std::make_unique<vector<IntVector>>(numVertices);
+        this -> adj = std::make_unique<vector<AdjacentWeightVector>>(numVertices);
         for (int i = 0; i < numVertices; ++i) {
-            (*adj)[i] = std::make_unique<std::vector<int>>();
+            (*adj)[i] = std::make_unique<std::vector<pair<int, int>>>();
         }
         this -> numVertices = numVertices;
     }
 
-    void insertEdge(const int& u, const int& v) {
-        (*adj)[u] -> push_back(v);
+    void insertEdge(const int& u, const int& v, const int& weight = 1) {
+        (*adj)[u - 1] -> push_back(std::make_pair(v - 1, weight));
         if (!directed)
-            (*adj)[v] -> push_back(u);
+            (*adj)[v - 1] -> push_back(std::make_pair(u - 1, weight));
     }
 
     void display() const {
         for (int i = 0; i < numVertices; ++i) {
-            std::cout << i << ": ";
+            std::cout << i + 1 << ":   ";
             for (const auto& v : *(*adj)[i]) {
-                std::cout << v << " -> ";
+                std::cout << v.first + 1 << ":" << v.second << " -> ";
             }
             std::cout << '\n';
         }
@@ -62,12 +77,11 @@ public:
             int current = queue.front();
             if (current == target)
                 return true;
-            std::cout << "current: " << current << '\n';
             queue.pop();
             for (const auto& adjacent : *(*adj)[current]) {
-                if (!visited[adjacent]) {
-                    visited[adjacent] = true;
-                    queue.push(adjacent);
+                if (!visited[adjacent.first]) {
+                    visited[adjacent.first] = true;
+                    queue.push(adjacent.first);
                 }
             }
         }
@@ -81,22 +95,64 @@ public:
         dfsUtil(start, target, visited, res);
         return res;
     }
+
+    int numEdges() const {
+        int res = 0;
+        for (const auto& list : *adj) {
+            res += list -> size();
+        }
+        return directed ? res : (res / 2);
+    }
+
+    vector<pair<int, int>> prim() const {
+        const EdgeWeightComparator edgeWeightComparator;
+        auto minHeap = std::make_unique<PriorityQueue<pair<int, int>, EdgeWeightComparator>>(edgeWeightComparator);
+        vector<bool> visited(numVertices, false);
+        vector<int> dist(numVertices, INT_MAX);
+        minHeap -> insert(std::make_pair(0, 0));
+        vector<int> prev(numVertices, -1);
+        while (!minHeap -> empty()) {
+            int u = minHeap -> extractTop().first;
+            if (visited[u]) {
+                continue;
+            }
+            visited[u] = true;
+            for (const auto& adjacentPair : *(*adj)[u]) {
+                int v = adjacentPair.first;
+                int weight = adjacentPair.second;
+                if (weight < dist[v] && !visited[v]) {
+                    dist[v] = weight;
+                    minHeap -> insert(adjacentPair);
+                    prev[v] = u;
+                }
+            }
+        }
+        vector<pair<int, int>> edges;
+        for (int i = 0; i < numVertices; ++i) {
+            if (prev[i] >= 0)
+                edges.push_back(std::make_pair(prev[i], i));
+        }
+        return edges;
+    }
 };
 
 int main() {
     Graph g(false, 7);
-    g.insertEdge(0, 1);
-    g.insertEdge(0, 3);
-    g.insertEdge(1, 2);
-    g.insertEdge(2, 3);
-    g.insertEdge(2, 4);
-    g.insertEdge(2, 5);
-    g.insertEdge(3, 4);
-    g.insertEdge(4, 5);
-    g.insertEdge(4, 6);
+    g.insertEdge(1, 6, 5);
+    g.insertEdge(1, 2, 4);
+    g.insertEdge(2, 7, 6);
+    g.insertEdge(2, 3, 9);
+    g.insertEdge(3, 4, 10);
+    g.insertEdge(4, 5, 12);
+    g.insertEdge(4, 7, 11);
+    g.insertEdge(5, 7, 16);
+    g.insertEdge(5, 6, 18);
 
     g.display();
 
-    std::cout << (g.dfs(0, 6) ? "true" : "false") << '\n';
+    auto edges = g.prim();
+    for (const auto& edge : edges) {
+        std::cout << "(" << edge.first + 1 << ", " << edge.second + 1 << ")\n";
+    }
     return 0;
 }
---
+++ priority-queue.h
@@ -0,0 +1,110 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using std::vector;
+
+template <typename T, typename Comparator>
+class PriorityQueue {
+private:
+    vector<T>* v;
+    Comparator comp;
+
+    void siftDown(int i) {
+        int largest, left, right;
+        while ((2 * i + 1) < v -> size()) {
+            largest = i;
+            left = 2 * i + 1;
+            right = 2 * i + 2;
+            if (left < v -> size() && comp((*v)[left], (*v)[largest]) > 0) {
+                largest = left;
+            }
+            if (right < v -> size() && comp((*v)[right], (*v)[largest]) > 0) {
+                largest = right;
+            }
+            if (largest == i) break;
+            std::swap((*v)[i], (*v)[largest]);
+            i = largest;
+        }
+    }
+
+    void heapify() {
+        for (int i = v -> size() / 2 - 1; i >= 0; --i) {
+            siftDown(i);
+        }
+    }
+
+    void siftUp() {
+        int i = v -> size() - 1;
+        while (i > 0) {
+            int parent = (i - 1) / 2;
+            if (comp((*v)[parent], (*v)[i]) > 0) 
+                break;
+            std::swap((*v)[parent], (*v)[i]);
+            i = parent;
+        }
+    }
+public:
+    explicit PriorityQueue(const Comparator& comp): v(new vector<T>()), comp(comp) {}
+
+    PriorityQueue(vector<int>*& v, const Comparator& comp): v(v), comp(comp) {
+        heapify();
+    }
+
+    ~PriorityQueue() {
+        delete v;
+    }
+
+    bool empty() const {
+        return v -> empty();
+    }
+
+    T top() const {
+        return (*v)[0];
+    }
+
+    T extractTop() {
+        T top = (*v)[0];
+        (*v)[0] = v -> back();
+        v -> pop_back();
+        siftDown(0);
+        return top;
+    }
+
+    template <typename Display>
+    void display(Display display) const {
+        for (const auto& x : *v) 
+            std::cout << "x: " << display(x) << '\n';
+    }
+    
+    void insert(const T val) {
+        v -> push_back(val);
+        int lastElemm = v -> size() - 1;
+        std::swap((*v)[0], (*v)[lastElemm]);
+        siftUp();
+    } 
+
+    int size() const {
+        return v -> size();
+    }
+};
+
+struct GreaterThan {
+    int operator() (const int& x1, const int& x2) {
+        return x1 - x2;
+    }
+};
+
+struct LessThan {
+    int operator() (const int& x1, const int& x2) {
+        return x2 - x1;
+    }
+};
+
+typedef PriorityQueue<int, LessThan> MinHeap;
+typedef PriorityQueue<int, GreaterThan> MaxHeap;
+
+#endif // PRIORITY_QUEUE_H
